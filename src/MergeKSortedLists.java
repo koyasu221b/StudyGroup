6a98216410f3a4d310bad2cffd9f26e6cc4e9090
Will Huang <will_huang@trend.com.tw>
2018-06-16 23:34:19 +0800

    MergeKSortedLists

--- src/MergeKSortedLists.java
+++ src/MergeKSortedLists.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class MergeKSortedLists {
+
+
+    private Comparator<ListNode> listNodeComparator = new Comparator<ListNode>() {
+        @Override
+        public int compare(ListNode o1, ListNode o2) {
+            return o1.val - o2.val;
+        }
+    };
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        if(lists == null || lists.length == 0){
+            return null;
+        }
+
+        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(
+                lists.length, listNodeComparator);
+
+        for(int i = 0 ; i < lists.length; i++){
+            if(lists[i] != null){
+                priorityQueue.add(lists[i]);
+            }
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+
+        while (!priorityQueue.isEmpty()){
+           ListNode node = priorityQueue.poll();
+           head.next = node;
+           head = head.next;
+
+           if(node.next != null){
+               priorityQueue.add(node.next);
+           }
+
+        }
+
+        return dummy.next;
+
+    }
 }
